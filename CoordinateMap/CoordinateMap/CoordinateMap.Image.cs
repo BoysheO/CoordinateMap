e653abd70701b46d07ab18c8a247ddf1ae91ba81
boysheo <boysheo@outlook.com>
2021-11-26 16:03:31 +0800

    add span and span2d supported

---
+++ CoordinateMap/CoordinateMap/CoordinateMap.Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Toolkit.HighPerformance;
+namespace com.boysheo.toolkit
+{
+    partial class CoordinateMap<T>
+    {
+        /// <summary>
+        /// 输出2D布局的数组
+        /// </summary>
+        public Span2D<T> AsSpan2D=>new Span2D<T>(Data.Array,Height,Width);
+
+        /// <summary>
+        /// 输出1D布局数组
+        /// </summary>
+        public Span<T> AsSpan => Data.AsSpan();
+    }
+}
